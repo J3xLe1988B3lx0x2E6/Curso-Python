4e4515099b16b94e7ccbcd5972c0844a07cd5ef4
JohnKun136NVCP <73410627+JohnKun136NVCP@users.noreply.github.com>
2023-08-06 22:15:53 -0600

    Agregado GoogleApi(Youtube) y script para credenciales

---
+++ Ejemplos/Avanzado/Apis/apirequest.py
@@ -0,0 +1,16 @@
+def CallTwitter():
+    consumerKey = ''
+    BrTKey =r''
+    consumerkeyS = ''
+    accessToken = ''
+    accessTokenSecret  =''
+    clientID=''
+    clientSecretID=''
+    return consumerKey, BrTKey,consumerkeyS,accessToken,accessTokenSecret,clientID,clientSecretID
+def CallGoogle():
+    pythonTokenKey = ''
+    return pythonTokenKey
+def CallSpoty():
+    clientID = ''
+    clientIDS = ''
+    return clientID,clientIDS
